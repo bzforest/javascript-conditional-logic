02905389916e81a882f4940eba80f819d70d90f2
Best <bestsakditat@gmail.com>
2025-12-12 15:24:57 +0700

    Add: hackhours exercise by Best

--- ex-3.js
+++ ex-3.js
@@ -3,3 +3,14 @@
 let lightBulbStatus = "Off";
 
 //Start coding here
+// if (lightBulbStatus === "on") {
+//     console.log("Light bulb is On.");
+// } else if (lightBulbStatus === "off") {
+//     console.log("Light bulb is Off.")
+// };
+
+lightBulbStatus === "On"
+? console.log("Light bulb is On.")
+: lightBulbStatus === "Off"
+? console.log("Light bulb is Off.")
+: console.log("Please choose the correct input (On/Off)");
--- ex-4.js
+++ ex-4.js
@@ -2,4 +2,23 @@
 // Switch Statement
 let lightBulbStatus = "On";
 
-//Start coding here
+// if (lightBulbStatus === "On") {
+//     console.log("Light bulb is On.");
+// } else if (lightBulbStatus === "off") {
+//     console.log("Light bulb is Off.")
+// } else if (lightBulbStatus === "Broken") {
+//     console.log("Light bulb is Broken.")
+// } else {
+//     console.log("Please choose the correct inout (On/Off/Broken)")
+
+switch (lightBulbStatus) {
+    case "On":
+        console.log("Light bulb is On.");
+        break;
+    case "Off":
+        console.log("Light bulb is Off.");
+        break;
+    case "Broken":
+        console.log("Light bulb is Broken.")
+        break;
+}
